6559150c6ed794edb7b6c38050e02ea152d307b8
Karlauer <73290377+Karlauer@users.noreply.github.com>
2022-07-08 15:30:43 +0200

    Finished the assignement

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // Function that randomly generates the Computers choices
 function computerPlay() {
-    let elements = ["Rock","Paper","Scissor"];
+    let elements = ["rock","paper","scissor"];
     return elements[Math.floor(Math.random() * 3)];
 }
 
@@ -10,43 +10,78 @@ function wincon(a,b) {
         return "Draw"
     }
     //Rock v Paper
-    else if ((a=="Rock" && b=="Paper" )|| (b=="Rock" && a=="Paper" )) {
-        return "Paper";
+    else if ((a=="rock" && b=="paper" )|| (b=="rock" && a=="paper" )) {
+        return "paper";
     }
     //Rock v Scissor
-    else if ((a=="Rock" && b=="Scissor" )|| (b=="Rock" && a=="Scissor" )) {
-        return "Rock";
+    else if ((a=="rock" && b=="scissor" )|| (b=="rock" && a=="scissor" )) {
+        return "rock";
     }
-    //Scisssor Paper
-    else if ((a=="Scissor" && b=="Paper" )|| (b=="Scissor" && a=="Paper" )) {
-        return "Scissor";
+    //Scisssor v Paper
+    else if ((a=="scissor" && b=="paper" )|| (b=="scissor" && a=="paper" )) {
+        return "scissor";
     }
 }
 
-function game(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
   
     if (playerSelection==null) {
         return "Please Insert a choice";
     }
-   // Draw:
+
     else if (wincon(playerSelection,computerSelection)=="Draw") {
         return "Draw";
     }
- // Rock beats Paper:
+
     else if (wincon(playerSelection,computerSelection)==playerSelection) {
-        return "Player wins";
+        return "Player";
     }
 
     else if (wincon(playerSelection,computerSelection)==computerSelection) {
-        return "Computer wins";
+        return "Computer";
     }
 
 
 }
 
-const playerSelection = prompt("Deine Wahl");
-const computerSelection = computerPlay();
-console.log(playerSelection);
-console.log(computerSelection);
+function game() {
+    let compwins = 0;
+    let playerwins = 0;
 
-console.log(game(playerSelection, computerSelection));
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Deine Wahl").toLowerCase();
+        let computerSelection = computerPlay();
+        console.log("Player selected: " + playerSelection);
+        console.log("Computer selected: " + computerSelection);
+        let winner = playRound(playerSelection, computerSelection);
+
+        if (winner == "Player") {
+            playerwins += 1;
+            console.log(winner + " won the round.");
+        }
+
+        else if (winner == "Computer") {
+            compwins++;
+            console.log(winner + " won the round.");
+        }
+
+        else {
+            console.log("Draw!")
+        }
+        
+        console.log(`Counter for Computer: ${compwins}, counter for Player: ${playerwins}`)
+     } 
+    
+     if (compwins>playerwins) {
+        console.log("Computer won the game");
+     }
+
+     else if (compwins==playerwins) {
+        console.log("Neither won the game");
+     }
+     else {
+        console.log("Player won the game");
+     }
+}
+
+game();
